b413fe7e45da78903690c305935f8180fb378c26
Vinay Kumar Patel <157368385+vkp0007@users.noreply.github.com>
2025-10-22 19:46:31 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,8 @@ app.use(express.urlencoded({ extended: true }))
 
 databaseConnection();
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 app.get('/', (req, res) => {
-  
-  res.send('API is running...');
+   res.send('API is running and DB is connected!');
 });
 
 // Mount auth routes
