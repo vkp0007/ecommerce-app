e49d84f84ee8231acf0bb3dc7b191699582079e2
Vinay Kumar Patel <157368385+vkp0007@users.noreply.github.com>
2025-10-22 20:44:06 +0530

    Add headers to axios instance for JSON content

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://ecommerce-app-git-main-vinay-kumar-patels-projects.vercel.app/api', // your backend URL
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default axiosInstance;
