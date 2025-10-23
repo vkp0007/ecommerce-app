78a000cfda7b0516795e78d627caf77bf87be4e4
Vinay Kumar Patel <157368385+vkp0007@users.noreply.github.com>
2025-10-23 17:18:27 +0530

    Update axios base URL for API requests

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ecommerce-app-git-main-vinay-kumar-patels-projects.vercel.app/api', // your backend URL
+  baseURL: 'https://ecommerce-app-flame-theta.vercel.app/api', // your backend URL
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
