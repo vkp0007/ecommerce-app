93b3f76bfa9dbde06a313c2573cb2c931bef61d0
Vinay Kumar Patel <157368385+vkp0007@users.noreply.github.com>
2026-03-06 18:32:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,16 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to DB
-databaseConnection();
-
+// DB connection
+app.use(async (req, res, next) => {
+  try {
+    await databaseConnection();
+    next();
+  } catch (err) {
+    console.error("❌ DB Connection failed:", err.message);
+    res.status(500).json({ message: "Database connection error" });
+  }
+});
 app.get('/', (req, res) => {
   res.send('API is running and DB is connected!');
 });
