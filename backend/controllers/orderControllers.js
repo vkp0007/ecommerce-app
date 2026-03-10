87e49a52f7878891761709f9dc4ff762fe76ed79
Vinay Kumar Patel <157368385+vkp0007@users.noreply.github.com>
2026-03-10 21:07:59 +0530

    Remove 'no orders found' check from getOrders
    Removed check for empty orders array and its response.

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -4,8 +4,6 @@ import { Order } from "../models/order.model.js";
 const getOrders = async (req, res) => {
   try {
     const orders = await Order.find({ userId: req.user._id });
-    if (!orders.length)
-      return res.status(404).json({ message: 'No orders found for this user' });
     res.status(200).json(orders);
   } catch (error) {
     res.status(500).json({ message: error.message });
