c7bf6db963960ce06292344575ed68db2f79364f
Vinay Kumar Patel <vinayptl47@gmail.com>
2026-03-11 01:00:54 +0530

    updated frontend domain

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-    'https://ecommerce-app-6f2b.vercel.app',
+    'https://mini-store-mu-mauve.vercel.app/',
       'http://localhost:5173'// your frontend domain
       
     ],
